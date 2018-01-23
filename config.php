95383bb88251eb2d36d14adfde99e533f2a28441
Peter Kelly <peterk1992@gmail.com>
2018-01-23 18:54:48 +0000

    Sync

---
+++ config.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * ======================================
+ *  Backup Your WHM Configuration File
+ * ======================================
+ */
+
+$license_key = '';
+
+$whm_hostname = '';
+$whm_port = '';
+$whm_username = '';
+$whm_auth = '';
+$whm_auth_key = '';
+
+$type_of_backup = '';
+$backup_criteria = '';
+$backup_exclusions = '';
+
+
+$backup_destination = '';
+$backup_server = '';
+$backup_port = '';
+$backup_user = '';
+$backup_pass = '';
+$backup_email = '';
+$backup_rdir = '';
+
+$backup_log = '';
+
+?>
